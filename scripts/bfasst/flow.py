4535bfb9b7364047b06963fb78c04f8de0ebd8da
Eli Cahill <ecahill42@gmail.com>
2019-12-17 16:56:43 -0700

    Added flow for yosys teched to synplify to IC2

--- scripts/bfasst/flow.py
+++ scripts/bfasst/flow.py
@@ -10,13 +10,15 @@ class Flows(enum.Enum):
     IC2_LSE_CONFORMAL = "IC2_lse_conformal"
     IC2_SYNPLIFY_CONFORMAL = "IC2_synplify_conformal"
     YOSYS_TECH_LSE_CONFORMAL = "yosys_tech_lse_conformal"
+    YOSYS_TECH_SYNPLIFY_CONFORMAL = "yosys_tech_synplify_conformal"
 
 
 # This uses a lambda so that I don't have to define all of the functions before this point
 flow_fcn_map = {
     Flows.IC2_LSE_CONFORMAL: lambda: flow_ic2_lse_conformal,
     Flows.IC2_SYNPLIFY_CONFORMAL: lambda: flow_ic2_synplify_conformal,
-    Flows.YOSYS_TECH_LSE_CONFORMAL: lambda: flow_yosys_tech_lse_conformal
+    Flows.YOSYS_TECH_LSE_CONFORMAL: lambda: flow_yosys_tech_lse_conformal,
+    Flows.YOSYS_TECH_SYNPLIFY_CONFORMAL: lambda: flow_yosys_tech_synplify_conformal
 }
 
 def get_flow_fcn_by_name(flow_name):
@@ -149,6 +151,46 @@ def flow_yosys_tech_lse_conformal(design, build_dir):
     # Try fixing the netlist LUT inits (there's some issue with how LSE
     #   generates them)
     lse_opt_tool.fix_lut_inits(design)
+    
+    # Run IC2 Implementation
+    impl_tool = bfasst.impl.ic2.IC2_ImplementationTool(build_dir)
+    status = impl_tool.implement_bitstream(design)
+    if status.error:
+        return status
+
+    # Run icestorm bitstream reversal
+    reverse_bit_tool = bfasst.reverse_bit.icestorm.Icestorm_ReverseBitTool(
+        build_dir)
+    status = reverse_bit_tool.reverse_bitstream(design)
+    if status.error:
+        return status
+
+    # Run conformal
+    compare_tool = bfasst.compare.conformal.Conformal_CompareTool(build_dir)
+    status = compare_tool.compare_netlists(design)
+    if status.error:
+        return status
+
+    return status
+    
+    print(build_dir)
+
+def flow_yosys_tech_synplify_conformal(design, build_dir):
+    # Run the Yosys synthesizer
+    yosys_synth_tool = bfasst.synth.yosys.Yosys_Tech_SynthTool(build_dir)
+    status = yosys_synth_tool.create_netlist(design)
+    if status.error:
+        return status
+
+    # Now run the Synplify synthesizer on the Yosys output
+    yosys_netlist_path = design.netlist_path
+    design.compare_golden_files.append(yosys_netlist_path.name)
+    design.compare_golden_files_paths.append(yosys_netlist_path)
+    design.golden_is_verilog = True
+    synp_opt_tool = bfasst.opt.ic2_synplify.IC2_Synplify_OptTool(build_dir)
+    status = synp_opt_tool.create_netlist(design, [str(yosys_netlist_path)], [])
+    if (status.error):
+        return status
     
     # Run IC2 Implementation
     impl_tool = bfasst.impl.ic2.IC2_ImplementationTool(build_dir)
