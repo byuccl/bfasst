bfa42db110ecf0144a43f6abdb8a1c53b172ecba
Jeff Goeders <jeff.goeders@gmail.com>
2020-10-19 14:59:32 -0600

    Restructure design.yaml parsing

--- scripts/bfasst/design.py
+++ scripts/bfasst/design.py
@@ -53,26 +53,87 @@ class Design:
         self.error_flow_names = []
         self.nets_to_remove_from_pcf = set()
 
-        if not os.path.isfile(self.yaml_path):
-            bfasst.utils.error("Design YAML file", self.yaml_path, "does not exist")
+        if not self.yaml_path.is_file():
+            error("Design YAML file", self.yaml_path, "does not exist")
 
         with open(self.yaml_path) as fp:
             design_props = yaml.safe_load(fp)
 
+        for k,v in design_props.items():
+            # Get top module name
+            if k == "top":
+                self.top = v
+
+            # Top file 
+            elif k == "top_file":
+                self.top_file_path = self.path / v
+                if not self.top_file_path.is_file():
+                    error("top_file", self.top_file_path, "does not exist.")
+
+            # Top architecture
+            elif k ==  "top_architecture":
+                self.top_architecture = v
+
+            # Synthesized netlist path
+            elif k == "synthesized_netlist":
+                self.netlist_path = self.path / v
+                if not self.netlist_path.is_file():
+                    error("synthesized_netlist file", self.netlist_path, "does not exist.")
+
+            # Reversed netlist path
+            elif k == "reversed_netlist":
+                self.reversed_netlist_path = self.path / v
+                if not self.reversed_netlist_path.is_file():
+                    error("reversed_netlist file", self.reversed_netlist_path, "does not exist.")
+
+            # Handle 'include_all_verilog_files' option
+            elif k == "include_all_verilog_files":
+                if v:
+                    for dir_item in self.path.iterdir():
+                        if (
+                            dir_item.is_file()
+                            and dir_item.suffix == ".v"
+                            and dir_item != self.top_file_path
+                        ):
+                            self.verilog_file_paths.append(dir_item)
+            
+            # Handle 'include_all_vhdl_files'
+            elif k == "include_all_vhdl_files":
+                if v:
+                    for dir_item in self.path.iterdir():
+                        if (
+                            dir_item.is_file()
+                            and dir_item.suffix == ".vhd"
+                            and dir_item != self.top_file_path
+                        ):
+                            self.vhdl_file_paths.append(dir_item)
+
+            # Add user-specified individual verilog files
+            elif k == "verilog_files":
+                for verilog_file in v:
+                    verilog_file_path = self.path / verilog_file
+                    if not verilog_file_path.is_file():
+                        error("Invalid 'verilog_files' option. File not found:", verilog_file_path)
+                    self.verilog_file_paths.append(verilog_file_path)
+
+            # VHDL libraries
+            elif k == "vhdl_libs":
+                for vhdl_lib in v:
+                    vhdl_lib_path = self.path / vhdl_lib
+                    for vhdl_file_path in vhdl_lib_path.rglob("*.vhd"):
+                        self.vhdl_libs[vhdl_file_path] = vhdl_lib_path.name
+
+            else:
+                error("Invalid option", k, "in", self.yaml_path)
+
+        ########### Validation #############
+
+        # Check for top
         if "top" not in design_props:
             bfasst.utils.error(self.yaml_path, "missing <top> property.")
-        self.top = design_props["top"]
 
-        # If top file is specified, make sure it can be found
-        if "top_file" in design_props:
-            top_file = design_props["top_file"]
-            self.top_file_path = self.path / top_file
-
-            if not self.top_file_path.is_file():
-                error("Top file", self.top_file_path, "does not exist.")
-
-        # Otherwise look for a file given the 'top' name
-        else:
+        # If no top_file given, find one 
+        if self.top_file_path is None and not self.netlist_path:
             verilog_top_path = self.path / (self.top + ".v")
             vhdl_top_path = self.path / (self.top + ".vhd")
             if verilog_top_path.is_file():
@@ -91,69 +152,20 @@ class Design:
                 )
 
         # For VHDL designs, we need the name of the 'architecture' of the top-level entity
-        if "top_architecture" in design_props:
-            self.top_architecture = design_props["top_architecture"]
-
-        # Handle 'include_all_verilog_files' option
-        if (
-            "include_all_verilog_files" in design_props
-            and design_props["include_all_verilog_files"]
-        ):
-            for dir_item in self.path.iterdir():
-                if (
-                    dir_item.is_file()
-                    and dir_item.suffix == ".v"
-                    and dir_item != self.top_file_path
-                ):
-                    self.verilog_file_paths.append(dir_item)
-
-        # Handle 'include_all_vhdl_files'
-        if "include_all_vhdl_files" in design_props and design_props["include_all_vhdl_files"]:
-            for dir_item in self.path.iterdir():
-                if (
-                    dir_item.is_file()
-                    and dir_item.suffix == ".vhd"
-                    and dir_item != self.top_file_path
-                ):
-                    self.vhdl_file_paths.append(dir_item)
-
-        # Add user-specified individual verilog files
-        if "verilog_files" in design_props:
-            for verilog_file in design_props["verilog_files"]:
-                verilog_file_path = self.path / verilog_file
-                if not verilog_file_path.is_file():
-                    error("Invalid 'verilog_files' option. File not found:", verilog_file_path)
-                self.verilog_file_paths.append(verilog_file_path)
-
-        # VHDL libraries
-        # if "vhdl_lib_files" in design_props:
-        #     for vhdl_lib in design_props["vhdl_lib_files"]:
-        #         for (lib, files) in vhdl_lib.items():
-        #             for f in files:
-        #                 self.vhdl_libs[self.full_path / f] = lib
-        if "vhdl_libs" in design_props:
-            for vhdl_lib in design_props["vhdl_libs"]:
-                vhdl_lib_path = self.path / vhdl_lib
-                for vhdl_file_path in vhdl_lib_path.rglob("*.vhd"):
-                    self.vhdl_libs[vhdl_file_path] = vhdl_lib_path.name
-
-    # def top_is_verilog(self):
-    #     return self.top_file_path.suffix == ".v"
-
-    # def top_is_vhdl(self):
-    #     return self.top_file_path.suffix == ".vhd"
+        if self.is_source_hdl() and self.get_top_hdl_type() == HdlType.VHDL and self.top_architecture is None:
+            error("top_architecture not specified for VHDL design")
+        
+    def is_source_hdl(self):
+        return self.top_file_path is not None
 
     def get_top_hdl_type(self):
         return get_hdl_type(self.top_file_path)
 
-    # def top_path(self):
-    #     return os.path.join(self.full_path, self.top_file)
-
     def get_support_files(self):
         return self.verilog_file_paths + self.vhdl_file_paths
 
-    def reversed_netlist_filename(self):
-        return os.path.basename(self.reversed_netlist_path)
+    # def reversed_netlist_filename(self):
+    #     return os.path.basename(self.reversed_netlist_path)
 
     def last_modified_time(self):
         return max([os.path.getmtime(f) for f in (self.yaml_path, self.top_file_path)])
