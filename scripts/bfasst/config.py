5446d1fd4fbec7e1f2acfcb8cf1847881b264b84
Jeff Goeders <jeff.goeders@gmail.com>
2021-03-01 10:02:31 -0700

    Update vivado path to /tools

--- scripts/bfasst/config.py
+++ scripts/bfasst/config.py
@@ -11,7 +11,7 @@ CONFORMAL_REMOTE_WORK_DIR = Path("/fsj/squallzz/bfasst_work")
 CONFORMAL_REMOTE_LIBS_DIR = Path("/fsj/squallzz/bfasst_libs")
 CONFORMAL_REMOTE_PATH = Path("/ee2/Cadence/CONFRML152/bin/lec")
 
-VIVADO_BIN_PATH = Path("/opt/Xilinx/Vivado/2019.2/bin/vivado")
+VIVADO_BIN_PATH = Path("/tools/Xilinx/Vivado/2019.2/bin/vivado")
 
 I2C_LSE_TIMEOUT = 600.0
 YOSYS_TIMEOUT = 600.0
