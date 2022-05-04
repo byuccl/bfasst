8597ce8f04c891ed5988ae46b20363aae1eafdf6
Jake Edvenson <jakers.edvenson@gmail.com>
2022-05-04 09:01:15 -0600

    New working module with both flows

---
+++ examples/byu/shiftReg/shiftReg.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: BYU (Brigham Young University)
+// Engineer: Jake Edvenson
+// 
+// Create Date: 05/4/2022 
+// Design Name: shiftReg
+// Project Name: bfasst testing
+// Description:A simple register to shift values
+// 
+// Dependencies: None
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+//Works correctly
+
+module shiftReg(clk,start,rst,data,shift,left, out);
+input clk, start, rst, left;
+input [7:0] data, shift;
+output reg[15:0] out;
+
+always@(posedge clk) begin
+    if(rst)
+        out <= 8'b0;
+    else if(start)
+        if(left)
+            out <= data << shift;
+        else
+            out <= data >> shift;
+ end
+        
+endmodule
