88bd0b1890fbc53cbe0b1fa14c2a58050af0883a
Jake Edvenson <jakers.edvenson@gmail.com>
2022-08-18 11:45:55 -0600

    Added automatic Vivado

--- scripts/bfasst/compare/analyze_graph.py
+++ scripts/bfasst/compare/analyze_graph.py
@@ -2,12 +2,18 @@ from re import X
 import bfasst
 import bfasst.paths
 import subprocess
+import shutil
 from subprocess import Popen
 from pathlib import Path
+from bfasst.config import VIVADO_BIN_PATH
 
 '''A function to launch the graphs for designs that have already been tested. Mainly meant
 for checking designs that came back unequivalent to see what was wrong with them.'''
 def analyze_graphs(path, module):
+    impl_v = path / Path(module + "_impl.v")
+    impl_tb = path / Path(module + "_impl_tb.v")
+    reversed_v = path / Path(module + "_reversed.v")
+    reversed_tb = path / Path(module + "_reversed_tb.v")
     impl_vcd = path / Path(module + "_impl.vcd")
     reversed_vcd = path / Path(module + "_reversed.vcd")
     impl_tcl = path / Path(module + "_impl.tcl")
@@ -15,6 +21,8 @@ def analyze_graphs(path, module):
     impl_fst = path / Path(module + "_impl.vcd.fst")
     reversed_fst = path / Path(module + "_reversed.vcd.fst")
     diff = path / Path("diff.txt")
+    run_vivado = bfasst.paths.ROOT_PATH / Path("scripts/bfasst/compare_waveforms/run_vivado.py")
+    base_path = bfasst.paths.ROOT_PATH / Path("scripts/bfasst/compare_waveforms")
 
     if(diff.exists()):
         with diff.open() as file:
@@ -36,11 +44,25 @@ def analyze_graphs(path, module):
             y = str(y)
             wavefile.write("initial_window_x " + x + "\n")
             wavefile.write("initial_window_y " + y + "\n")
+        
+        choice = input("Compare with Vivado? 1 for yes, 0 for no.")
+        vivado = False
+        if(choice != "0"):
+            vivado=True
 
-    commands = [
-        ["gtkwave", "-T", str(impl_tcl), "-o", str(impl_vcd)],
-        ["gtkwave", "-T", str(reversed_tcl), "-o", str(reversed_vcd)]
-    ]
+    if(vivado):
+        commands = [
+            ["gtkwave", "-T", str(impl_tcl), "-o", str(impl_vcd)],
+            ["gtkwave", "-T", str(reversed_tcl), "-o", str(reversed_vcd)],
+            ["python", str(run_vivado), str(impl_v), 
+            str(impl_tb), module + "_impl_tb", str(reversed_v), str(reversed_tb), module + "_reversed_tb", str(VIVADO_BIN_PATH),
+            str(base_path)]
+        ]
+    else:
+        commands = [
+            ["gtkwave", "-T", str(impl_tcl), "-o", str(impl_vcd)],
+            ["gtkwave", "-T", str(reversed_tcl), "-o", str(reversed_vcd)]
+        ]
 
     procs = [Popen(i) for i in commands]
     for p in procs:
