a4b8258b74ce485cda82a4b411da08098738e043
Jonathan Thompson <jthompson3198@gmail.com>
2022-09-16 13:12:32 -0600

    Changes to run_experiment.py and creation of html

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+from glob import glob
 import pathlib
 import queue
 import sys
@@ -11,6 +12,8 @@ from threading import Thread
 import random
 import datetime
 import os
+from prettytable import from_html_one
+import json
 
 
 import bfasst
@@ -21,6 +24,7 @@ ljust = 0
 statuses = None
 running_list = None
 print_lock = None
+designs_list = []
 
 
 def print_running_list():
@@ -63,10 +67,121 @@ def update_runtimes_thread(num_jobs):
     ).start()
 
 
+
+def chk_html(html_path):
+    if os.path.getsize(os.path.abspath(html_path)) == 0:
+        tmp_html = open(html_path, "w")
+        tmp_html.write("<!DOCTYPE html>\n<html>\n<style>\ntable, th, td {\n\tborder:1px solid black;\n}\n</style>\n<body>\n\n<h2>BFASST Experiment Runs and Results</h2>\n")
+        tmp_html.write("<table>\n<thead>\n\t<tr>\n\t\t<th>Design</th>\n\t\t<th>IC2_lse_conformal</th>\n\t\t<th>IC2_synplify_conformal</th>\n\t\t<th>synplify_IC2_icestorm_onespin</th> \
+        \n\t\t<th>yosys_tech_lse_conformal</th>\n\t\t<th>yosys_tech_synplify_conformal</th>\n\t\t<th>yosys_tech_synplify_onespin</th>\n\t\t<th>yosys_synplify_error_onespin</th>\n\t\t \
+        <th>xilinx_conformal</th>\n\t\t<th>xilinx_conformal_impl</th>\n\t\t<th>xilinx_yosys_impl</th>\n\t\t<th>xilinx_yosys_waveform</th>\n\t\t<th>gather_impl_data</th>\n\t\t<th>conformal_only</th> \
+        \n\t\t<th>xilinx</th>\n\t</tr>\n</thead>\n<tbody>\n</tbody>\n</table>")
+        tmp_html.write("\n\n<p>To run these designs yourself, check out the <a href=\"https://github.com/byuccl/bfasst\">BFASST Github repository</a>.</p>\n\n</body>\n</html>")
+        tmp_html.close()
+
+
+
+def write_html(htmlStr, idx_html_path):
+    yes_str = "<td>Yes</td>"
+    no_str = "<td>No</td>"
+    yes_style = " style=\"background-color:darkseagreen;color:black;\""
+    no_style = " style=\"background-color:lightcoral;color:black;\""
+    while htmlStr.find(yes_str) != -1:
+        index = htmlStr.find(yes_str)
+        htmlStr = htmlStr[:index+3] + yes_style + htmlStr[index+3:]
+    while htmlStr.find(no_str) != -1:
+        index = htmlStr.find(no_str)
+        htmlStr = htmlStr[:index+3] + no_style + htmlStr[index+3:]
+
+
+    htmlFile = open(idx_html_path, "w")
+    htmlFile.write("<!DOCTYPE html>\n<html>\n<style>\ntable, th, td {\n\tborder:1px solid black;\n}\n</style>\n<body>\n\n<h2>BFASST Experiment Runs and Results</h2>\n")
+    htmlFile.write("\n" + htmlStr)
+    htmlFile.write("\n\n<p>To run these designs yourself, check out the <a href=\"https://github.com/byuccl/bfasst\">BFASST Github repository</a>.</p>\n\n</body>\n</html>")
+    htmlFile.close()
+
+
+
+def html_update(equivalence, design, flow_fcn):
+    flows_list = ["IC2_lse_conformal", "IC2_synplify_conformal", "synplify_IC2_icestorm_onespin", 
+    "yosys_tech_lse_conformal", "yosys_tech_synplify_conformal", "yosys_tech_synplify_onespin", 
+    "yosys_synplify_error_onespin", "xilinx_conformal", "xilinx_conformal_impl", "xilinx_yosys_impl", 
+    "xilinx_yosys_waveform", "gather_impl_data", "conformal_only", "xilinx"]
+
+    idx_html_path = "./index.html"
+
+    if str(equivalence).find("Equivalent") != -1:
+        table_value = "Yes"
+    elif str(equivalence).find("Not equivalent") != -1:
+        table_value = "No"
+    else:
+        table_value = "ERROR"
+
+    flow_name = flow_fcn.split()
+    curr_flow = flow_name[1][5:]
+
+    flow_dict = dict.fromkeys(flows_list, "Not Run Yet")
+    for flow in flows_list:
+        if flow == curr_flow:
+            flow_dict[flow] = table_value
+
+    chk_html(idx_html_path)
+    with open(idx_html_path, "r") as fp:
+        html = fp.read()
+
+    x = from_html_one(html)
+    x_json = json.loads(x.get_json_string())
+
+    if design in x.get_json_string():
+        for i in range(len(x_json)):
+            curr_x_json = x_json[i]
+            if design in str(curr_x_json):
+                if curr_x_json[curr_flow] != flow_dict[curr_flow] and flow_dict[curr_flow] != "Not Run Yet":
+                    curr_x_json[curr_flow] = flow_dict[curr_flow]
+                for f in flows_list:
+                    if f != curr_flow:
+                        flow_dict[f] = curr_x_json[f]
+                x.del_row(i-1)
+                break
+
+    temp_list = [flow_dict[column] for column in flows_list]
+    temp_list.insert(0, design)
+    x.add_row(temp_list)
+
+    write_html(x.get_html_string(), idx_html_path)
+
+
+
+def list_maker(design_dict={}, go=False):
+
+    global designs_list
+    idx_html_path = "./index.html"
+    if design_dict != {}:
+        file1 = open("temp.txt", "a+")  # append mode
+        json.dump(design_dict, file1)
+        file1.write("\n")
+        file1.close()
+    if go:
+        try:
+            file1 = open("temp.txt", "r")
+            line = file1.readline()
+            while line:
+                json_temp = json.loads(line)
+                line = file1.readline()
+                html_update(json_temp["status"], json_temp["design"], json_temp["flow_fcn"])
+            file1.close()
+            os.remove("temp.txt")
+        except:
+            chk_html(idx_html_path)
+            print("index.html unchanged") # Do nothing
+
+
+
 def run_design(design, design_dir, flow_fcn):
     """ This function runs a single job, running the selected CAD flow for one design """
 
     global running_list
+    global designs_list
 
     running_list[design.rel_path] = datetime.datetime.now()
     print_running_list()
@@ -74,6 +189,10 @@ def run_design(design, design_dir, flow_fcn):
     # time.sleep(random.randint(1,2))
     # status = None
     status = flow_fcn(design, design_dir, print_to_stdout=False)
+    path = pathlib.PurePath(design_dir)
+    design_dict = {"design": path.name, "status": str(status), "flow_fcn": str(flow_fcn).casefold()}
+    design_dict_copy = design_dict.copy()
+    list_maker(design_dict=design_dict_copy)
     return (design, status)
 
 
@@ -114,6 +233,7 @@ def main():
     global statuses
     global running_list
     global print_lock
+    global designs_list
 
     parser = argparse.ArgumentParser()
 
@@ -240,6 +360,7 @@ def main():
     # print(types)
 
     print("-" * 80)
+    list_maker(go=True)
     print("Execution took", round(t_end - t_start,1), "seconds")
 
 
