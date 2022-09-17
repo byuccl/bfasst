c87d4d89afb7a463b41910fa0fe460795be07dd4
ReillyMcK <romckend@byu.edu>
2022-09-17 02:15:07 -0600

    Support flow_args in synthesis, special logic for ooc arg

--- scripts/bfasst/synth/vivado.py
+++ scripts/bfasst/synth/vivado.py
@@ -15,11 +15,13 @@ from bfasst.tool import ToolProduct
 class Vivado_SynthesisTool(SynthesisTool):
     TOOL_WORK_DIR = "vivado_synth"
 
+
     def create_netlist(self, design):
+        default = "out_of_context" not in self.flow_args
         log_path = self.work_dir / bfasst.config.SYNTH_LOG_NAME
 
         # Save edif netlist path to design object
-        design.netlist_path = self.cwd / (design.top + ".edf")
+        design.netlist_path = self.cwd / f"{design.top}.edf"
         design.constraints_path = self.cwd / "constraints.xdc"
 
         generate_netlist = ToolProduct(design.netlist_path, log_path, self.check_synth_log)
@@ -53,7 +55,8 @@ class Vivado_SynthesisTool(SynthesisTool):
             raise e
 
         # Extract contraint file from Vivado-assigned pins
-        self.extract_contraints(design, report_io_path)
+        if default:
+            self.extract_contraints(design, report_io_path)
 
         # Check synthesis log
         status = self.check_synth_log(log_path)
@@ -61,49 +64,56 @@ class Vivado_SynthesisTool(SynthesisTool):
         return self.success_status
 
     def run_synth(self, design, log_path, report_io_path):
-        tcl_path = self.work_dir / ("synth.tcl")
+        tcl_path = self.work_dir / "synth.tcl"
 
         with open(tcl_path, "w") as fp:
             if design:
-                fp.write("if { [ catch {\n")
-                fp.write("set_part " + bfasst.config.PART + "\n")
+                fp.write(f"if {{ [ catch {{\n")
+                fp.write(f"set_part {bfasst.config.PART}\n")
 
                 if design.get_top_hdl_type() == HdlType.VERILOG:
-                    fp.write("read_verilog " + str(design.top_file_path) + "\n")
+                    fp.write(f"read_verilog {design.top_file_path}\n")
                 elif design.get_top_hdl_type() == HdlType.SYSTEM_VERILOG:
-                    fp.write("read_verilog -sv " + str(design.top_file_path) + "\n")
+                    fp.write(f"read_verilog -sv {design.top_file_path}\n")
                 else:
-                    fp.write("read_vhdl " + str(design.top_file_path) + "\n")
+                    fp.write(f"read_vhdl {design.top_file_path}\n")
                 
                 # Add verilog files
                 for vf in design.verilog_file_paths:
-                    fp.write("read_verilog " + str(vf) + "\n")
+                    fp.write(f"read_verilog {vf}\n")
                 
                 # Add system verilog files
                 for vf in design.system_verilog_file_paths:
-                    fp.write("read_verilog -sv " + str(vf) + "\n")
+                    fp.write(f"read_verilog -sv {vf}\n")
                 
                 # Add VHDL files
                 for vf in design.vhdl_file_paths:
-                    fp.write("read_vhdl " + str(vf) + "\n")
+                    fp.write(f"read_vhdl {vf}\n")
 
                 # Add vhdl library files
                 for vf, libname in design.vhdl_libs.items():
-                    fp.write("read_vhdl -library " + libname + " " + str(vf) + "\n")
+                    fp.write(f"read_vhdl -library " + libname + " {vf}\n")
 
+                if not self.flow_args:
                 # Synthesize - do not include any DSP modules in the synthesized design
-                fp.write("synth_design -top " + design.top + " -max_dsp 0" + "\n")
+                    fp.write(f"synth_design -top " + design.top + " -max_dsp 0" + "\n")
+                else:
+                    top = f" -top {design.top} " if "top" not in self.flow_args else ""
+                    dsp = " -max_dsp 0 " if "max_dsp" not in self.flow_args else ""
+                    fp.write(f"synth_design{top}{dsp} {self.flow_args}")
 
-                # Auto-place ports
-                fp.write("place_ports\n")
-                fp.write("write_edif -force {" + str(design.netlist_path) + "}\n")
-                fp.write("write_checkpoint -force -file " + str(self.work_dir / "design.dcp") + "\n")
+                if "out_of_context" not in self.flow_args:
+                    # Auto-place ports
+                    fp.write(f"place_ports\n")
+                fp.write(f"write_edif -force {{{design.netlist_path}}}\n")
+                fp.write(f"write_checkpoint -force -file {self.work_dir / 'design.dcp'}\n")
 
-                # Save IO to determine where auto port placement occurred
-                fp.write("report_io -force -file " + str(report_io_path) + "\n")
+                if "out_of_context" not in self.flow_args:
+                    # Save IO to determine where auto port placement occurred
+                    fp.write(f"report_io -force -file {report_io_path}\n")
 
-                fp.write("} ] } { exit 1 }\n")
-                fp.write("exit\n")
+                fp.write(f"}} ] }} {{ exit 1 }}\n")
+                fp.write(f"exit\n")
 
         with open(log_path, "w") as fp:
             cmd = [str(VIVADO_BIN_PATH), "-mode", "tcl", "-source", str(tcl_path)]
