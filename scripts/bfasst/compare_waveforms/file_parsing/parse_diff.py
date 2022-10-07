05b87812237b588e31d279022ec3ed0e95408b6c
Jake Edvenson <jakers.edvenson@gmail.com>
2022-10-07 07:52:06 -0600

    Comprehensive diff beta

--- scripts/bfasst/compare_waveforms/file_parsing/parse_diff.py
+++ scripts/bfasst/compare_waveforms/file_parsing/parse_diff.py
@@ -1,64 +1,130 @@
 from pathlib import Path
 import subprocess
+import math
 
+def check_duplicates(line, data, write):
+    if line[0] is "0" or line[0] is "1":
+        if line[1 : len(line) - 1] in data["binary_signal"]:
+            j = data["binary_signal"].index(line[1 : len(line) - 1])
+            if data["binary_state"][j] != line[0]:
+                write.write(line)
+            data["binary_state"][j] = line[0]
+    elif " " in line:
+        if (
+            line[line.index(" ") + 1 : len(line) - 1]
+        ) in data["signal"]:
+            j = data["signal"].index(
+                line[line.index(" ") + 1 : len(line) - 1]
+            )
+            if data["state"][j] != line[0 : line.index(" ")]:
+                write.write(line)
+            data["state"][j] = line[0 : line.index(" ")]
+    elif line[0] is "#":
+        data["time"] = (line[1:len(line)-1])
+        write.write(line)
+    else:
+        write.write(line)
+    return(data)
+
+def past_initial_data(line, data):
+    if data["past_header"] is True:
+        if "$dumpvars" in line:
+            data["past_definitions"] = True
+        else:
+            if (
+                "$upscope" not in line
+                and "$enddefinitions" not in line
+                and "#0" not in line
+                ):
+                data["returned_signal"] = parse_signals(line)
+                if data["returned_signal"][0] is "1":
+                    data["binary_name"].append(data["returned_signal"][2])
+                    data["binary_signal"].append(data["returned_signal"][1])
+                    data["binary_state"].append("0")
+                else:
+                    data["name"].append(data["returned_signal"][2])
+                    data["signal"].append(data["returned_signal"][1])
+                    data["state"].append("b0")
+    if "$scope module" in line:
+        data["past_header"] = True
+    return(data)
 
 def check_diff(paths):
-    """This function will run through all of the differences between the two VCD files and 
-    then determine equivalence based upon the number of different lines."""
+    """This function will run through all of the differences between the two VCD files and then determine equivalence based upon the number of different lines."""
     is_equivalent = False
+    time_related_data = []
     for i in range(2):
+        data = {}
         signal = []
+        name = []
         binary_signal = []
-        state = []
         binary_state = []
-        returned_signal = []
-        past_header = False
-        past_definitions = False
+        binary_name = []
+        state = []
+        data["time"] = '0'
+        data["name"] = []
+        data["binary_name"] = []
+        data["signal"] = []
+        data["binary_signal"] = []
+        data["state"] = []
+        data["binary_state"] = []
+        data["returned_signal"] = []
+        data["past_header"] = False
+        data["past_definitions"] = False
+        curr_time = '0'
         if paths["temp_vcd"][i].exists():
             paths["temp_vcd"][i].unlink()
         with paths["vcd"][i].open("r") as file:
             with paths["temp_vcd"][i].open("x") as write:
                 for line in file:
-                    if past_header is True:
-                        if past_definitions is False:
-                            if "$dumpvars" in line:
-                                past_definitions = True
-                            else:
-                                if (
-                                    "$upscope" not in line
-                                    and "$enddefinitions" not in line
-                                    and "#0" not in line
-                                ):
-                                    returned_signal = parse_signals(line)
-                                    if returned_signal[0] is "1":
-                                        binary_signal.append(returned_signal[1])
-                                        binary_state.append("0")
-                                    else:
-                                        signal.append(returned_signal[1])
-                                        state.append("b0")
-                        else:
-                            if line[0] is "0" or line[0] is "1":
-                                if line[1 : len(line) - 1] in binary_signal:
-                                    j = binary_signal.index(line[1 : len(line) - 1])
-                                    if binary_state[j] != line[0]:
-                                        binary_state[j] = line[0]
-                                        write.write(line)
-                            elif " " in line:
-                                if (
-                                    line[line.index(" ") + 1 : len(line) - 1]
-                                ) in signal:
-                                    j = signal.index(
-                                        line[line.index(" ") + 1 : len(line) - 1]
-                                    )
-                                    if state[j] != line[0 : line.index(" ")]:
-                                        state[j] = line[0 : line.index(" ")]
-                                        write.write(line)
-                            else:
-                                write.write(line)
-                    if "$scope module" in line:
-                        past_header = True
-
-    # Finds how many lines are different in the two files.
+                    if(data["past_definitions"] is False):
+                        data = past_initial_data(line, data)
+                    else:
+                        binary_signal = data["binary_signal"]
+                        signal = data["signal"]
+                        name = data["name"]
+                        binary_name = data["binary_name"]
+                        data = check_duplicates(line, data, write)
+                        if data["time"] != curr_time:
+                            for i in data["binary_state"]:
+                                binary_state.append(i)
+                            for i in data["state"]:
+                                state.append(i)
+                            curr_time = data["time"]
+        time_related_data.append([binary_state, state])
+    print(signal)
+    t=0
+    h=0
+    for i, j in zip(time_related_data[0][0], time_related_data[1][0]):
+        if i != j:
+            print(h)
+            print(binary_name[h])
+            print("not equivalent with")
+            print(i)
+            print(j)
+            print(int(math.ceil((t-h)/(len(binary_signal)))))
+            print("\n")
+        if h<len(binary_signal)-1:
+            h=h+1
+        else:
+            h=0
+        t = t + 1
+    t=0
+    h=0
+    for i, j in zip(time_related_data[0][1], time_related_data[1][1]):
+        if i != j:
+            print(h)
+            print(name[h])
+            print("not equivalent with")
+            print(i)
+            print(j)
+            print(int(math.ceil((t-h)/(len(signal)))))
+            print("\n")
+        if h<len(signal)-1:
+            h=h+1
+        else:
+            h=0
+        t = t + 1
     dif = subprocess.getoutput(
         [f"diff -c {paths['temp_vcd'][0]} {paths['temp_vcd'][1]}"]
     )
@@ -71,7 +137,8 @@ def check_diff(paths):
     lines = 0
     with paths["diff"].open("r") as file:
         for line in file:
-            lines = lines + 1
+            if len(line)!=0:
+                lines = lines + 1
     print(f"{lines} lines found non-equivalent!")
     # If there are more than 32 lines different, the two designs must be unequivalent.
     if lines > 0:
@@ -141,12 +208,7 @@ def parse_signals(line):
 
 
 def parse_diff(paths):
-    """This function reads through the diff.txt file generated by running a diff 
-    function on the two vcd files (text representation of a wave file).
-    Since the VCD files represent each signal as a symbol (IE: $, #, !, etc), this 
-    script replaces those names with the actual signal names.
-    This script also tells you the exact times, in ms, that these differences occur.
-    The output of this script is saved in the waveform folder AND is printed out anytime 
+    """This function reads through the diff.txt file generated by running a diff function on the two vcd files (text representation of a wave file). Since the VCD files represent each signal as a symbol (IE: $, #, !, etc), this script replaces those names with the actual signal names.This script also tells you the exact times, in ms, that these differences occur. The output of this script is saved in the waveform folder AND is printed out anytime 
     unequivalent waveforms are viewed."""
     words = []
     symbols = []
