9e6bdf49126e7189ac50acc1f0737ca83638fafd
Jake Edvenson <jakers.edvenson@gmail.com>
2022-10-11 15:15:06 -0600

    Now im just salty...

--- scripts/bfasst/compare_waveforms/tools/run_vivado.py
+++ scripts/bfasst/compare_waveforms/tools/run_vivado.py
@@ -6,21 +6,10 @@ import sys
 
 def main():
     """The main function."""
-    if(len(sys.argv)>7):
-        base_path = sys.argv[1]
-        tb_path = sys.argv[2]
-        tb_name = sys.argv[3]
-        base_path_r = sys.argv[4]
-        tb_path_r = sys.argv[5]
-        tb_name_r = sys.argv[6]
+    assert sys.argv[7] is not None, "VIVADO_PATH environmental variable was not set!"
 
-        vivado = sys.argv[7]
-        base = sys.argv[8]
-
-    assert vivado is not None, "VIVADO_PATH environmental variable was not set!"
-
-    template = Path(base) / ("templates/template.tcl")
-    temp_tcl = Path(base) / str("temp.tcl")
+    template = Path(sys.argv[8]) / ("templates/template.tcl")
+    temp_tcl = Path(sys.argv[8]) / str("temp.tcl")
     if temp_tcl.exists():
         temp_tcl.unlink()
 
@@ -29,56 +18,61 @@ def main():
             for line in file:
                 if "PATH" in line:
                     line = line.replace(
-                        line[line.find("PATH") : line.find("PATH") + 4], base_path
+                        line[line.find("PATH") : line.find("PATH") + 4], sys.argv[1]
                     )
                 if "FILE_T" in line:
                     line = line.replace(
-                        line[line.find("FILE_T") : line.find("FILE_T") + 6], tb_path
+                        line[line.find("FILE_T") : line.find("FILE_T") + 6], sys.argv[2]
                     )
                 elif "TB" in line:
                     line = line.replace(
-                        line[line.find("TB") : line.find("TB") + 2], tb_name
+                        line[line.find("TB") : line.find("TB") + 2], sys.argv[3]
                     )
                 output.write(line)
 
-    temp2_tcl = Path(base) / str("temp2.tcl")
+    temp2_tcl = Path(sys.argv[8]) / str("temp2.tcl")
     if temp2_tcl.exists():
         temp2_tcl.unlink()
 
     with template.open("r") as file:
         with temp2_tcl.open("x") as output:
             for line in file:
-                if "temp" in line:
-                    line = line.replace(
-                        line[line.find("temp") : line.find("temp") + 4], "temp2"
-                    )
-                if "PATH" in line:
-                    line = line.replace(
-                        line[line.find("PATH") : line.find("PATH") + 4], base_path_r
-                    )
-                if "FILE_T" in line:
-                    line = line.replace(
-                        line[line.find("FILE_T") : line.find("FILE_T") + 6], tb_path_r
-                    )
-                elif "TB" in line:
-                    line = line.replace(
-                        line[line.find("TB") : line.find("TB") + 2], tb_name_r
-                    )
+                line = parse_line(line)
                 output.write(line)
 
     commands = [
-        [vivado, "-nolog", "-nojournal", "-source", str(temp_tcl)],
-        [vivado, "-nolog", "-nojournal", "-source", str(temp2_tcl)],
+        [sys.argv[7], "-nolog", "-nojournal", "-source", str(temp_tcl)],
+        [sys.argv[7], "-nolog", "-nojournal", "-source", str(temp2_tcl)],
     ]
 
     procs = [Popen(i) for i in commands]
-    for p in procs:
-        p.wait()
+    for proc in procs:
+        proc.wait()
 
     shutil.rmtree("temp")
     shutil.rmtree("temp2")
     temp_tcl.unlink()
     temp2_tcl.unlink()
+
+def parse_line(line):
+    """Why does this exist? So pylint will be happy about branch numbers"""
+    if "temp" in line:
+        line = line.replace(
+            line[line.find("temp") : line.find("temp") + 4], "temp2"
+        )
+    if "PATH" in line:
+        line = line.replace(
+            line[line.find("PATH") : line.find("PATH") + 4], sys.argv[4]
+        )
+    if "FILE_T" in line:
+        line = line.replace(
+            line[line.find("FILE_T") : line.find("FILE_T") + 6], sys.argv[5]
+        )
+    elif "TB" in line:
+        line = line.replace(
+            line[line.find("TB") : line.find("TB") + 2], sys.argv[6]
+        )
+    return(line)
 
 if __name__ == "__main__":
     main()
