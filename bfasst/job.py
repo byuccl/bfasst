23ec595c6a81fed25e557efec0ea8913cbb9d9ad
KeenanRileyFaulkner <keenan.r.faulkner@gmail.com>
2023-06-26 17:34:41 -0600

    clarified names

--- bfasst/job.py
+++ bfasst/job.py
@@ -7,14 +7,14 @@ from bfasst.tool import BfasstException
 class Job:
     """Class for a job function and dependency list"""
 
-    def __init__(self, function, design_name, dependencies=None):
+    def __init__(self, function, design_rel_path, dependencies=None):
         self.function = function
-        self.design_name = design_name
+        self.design_rel_path = design_rel_path
         self.dependencies = dependencies
 
     def invert(self):
         """This can be called in the case where we want to invert the job's exception handling"""
-        return Job(self.inverter, self.design_name, self.dependencies)
+        return Job(self.inverter, self.design_rel_path, self.dependencies)
 
     def inverter(self):
         try:
