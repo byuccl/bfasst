5e9a1c02db1aa939e058cd0a79496d723460c94c
KeenanRileyFaulkner <keenan.r.faulkner@gmail.com>
2023-08-21 11:30:06 -0600

    Added comment to method for cleaning error injections and comparisons

--- bfasst/utils/general.py
+++ bfasst/utils/general.py
@@ -167,6 +167,7 @@ def clean_error_injections_and_comparisons(designs):
         error_dir = ROOT_PATH / "build" / design / "error_injection"
         for file in cmp_dir.iterdir():
             with open(file, "r") as f:
+                # SUCCESS means the compare tool did not detect an actual error
                 if "FAIL" not in f.read():
                     fail_list.append(file.name.split("_cmp.log")[0])
                 else:
