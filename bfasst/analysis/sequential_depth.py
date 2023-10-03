ccc1391641f5e0884ff156b60fd739e98a666144
Jeff Goeders <jeff.goeders@gmail.com>
2023-10-03 13:11:49 -0600

    working on sequential depth

--- bfasst/analysis/sequential_depth.py
+++ bfasst/analysis/sequential_depth.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from matplotlib import pyplot as plt
 import numpy as np
 import spydrnet as sdn
@@ -35,8 +36,9 @@ class SequentialDepthAnalysis(AnalysisTool):
         self.plot_histogram(instance_depths, "depths_max.png")
 
     def calculate_depths(self, netlist, max_not_min):
-        starting_points = []
         instance_depth = {}
+        instances_to_visit = []
+        dependencies = defaultdict(set)
 
         # Initialize instances connected to inputs
         self.log_title("Initializing instances connected to inputs")
@@ -57,83 +59,71 @@ class SequentialDepthAnalysis(AnalysisTool):
                 instance_sdn = pin.instance
                 instance = netlist.instance_wrappers[instance_sdn]
 
-                starting_points.append(instance)
+                instances_to_visit.append(instance)
                 instance_depth[instance] = 0
                 self.log(f"{instance.name} -> {instance_depth[instance]}")
 
                 # self.log(f"{pin.instance.name} -> {instance_depth[pin.instance]}")
 
-        current_depth = 0
+        while instances_to_visit:
+            instance = instances_to_visit.pop(0)
+            assert isinstance(instance, sdn_helpers.Instance)
+            instance_sdn = instance.instance
 
-        while starting_points:
-            self.log_title("Expanding depth", current_depth)
+            self.log("Visiting", instance_sdn.name, instance_sdn.reference.name)
 
-            if current_depth > 0:
-                self.log(f"Assigning FFs to depth {current_depth}")
-                for instance in starting_points:
-                    instance_depth[instance] = current_depth
-                    self.log(f"  {instance.name} -> {current_depth}")
+            # Iterate through ouput pins
+            for pin_out in instance_sdn.pins:
+                # We only care about outputs of instances
+                if isinstance(pin_out, sdn.InnerPin):
+                    continue
 
-            instances_to_visit = list(starting_points)
-            starting_points = set()
+                if (
+                    sdn_helpers.Net.get_direction_for_unisim(
+                        pin_out.instance.reference.name, pin_out.inner_pin.port.name
+                    )
+                    != sdn.OUT
+                ):
+                    continue
 
-            while instances_to_visit:
-                instance = instances_to_visit.pop(0)
-                assert isinstance(instance, sdn_helpers.Instance)
-                instance_sdn = instance.instance
+                # For each output pin, find all instances that it connects to
+                net = netlist.wire_to_net[pin_out.wire]
+                assert isinstance(net, sdn_helpers.Net)
 
-                self.log("Visiting", instance_sdn.name, instance_sdn.reference.name)
+                for pin in net.all_pins():
+                    if pin == pin_out or isinstance(pin, sdn.InnerPin):
+                        continue
+                    next_instance_sdn = pin.instance
+                    next_instance = netlist.instance_wrappers[next_instance_sdn]
 
-                # Iterate through ouput pins
-                for pin_out in instance_sdn.pins:
-                    # We only care about outputs of instances
-                    if isinstance(pin_out, sdn.InnerPin):
+                    if next_instance.is_assign():
                         continue
 
-                    if (
-                        sdn_helpers.Net.get_direction_for_unisim(
-                            pin_out.instance.reference.name, pin_out.inner_pin.port.name
-                        )
-                        != sdn.OUT
+                    self.log(f"  {next_instance.name}")
+
+                    # Check for feedback
+                    if next_instance in dependencies[instance]:
+                        self.log("    Feedback detected")
+                        continue
+
+                    # Add dependencies to track feedback
+                    dependencies[next_instance].add(instance)
+                    dependencies[next_instance].update(dependencies[instance])
+
+                    # If this has already been visited, don't visit again,
+                    # unless we are looking for max depth and the depth is going to increase
+                    if next_instance in instance_depth and (
+                        not max_not_min
+                        or instance_depth[next_instance] >= instance_depth[instance] + 1
                     ):
                         continue
 
-                    # For each output pin, find all instances that it connects to
-                    net = netlist.wire_to_net[pin_out.wire]
-                    assert isinstance(net, sdn_helpers.Net)
-
-                    for pin in net.all_pins():
-                        if pin == pin_out or isinstance(pin, sdn.InnerPin):
-                            continue
-                        next_instance_sdn = pin.instance
-                        next_instance = netlist.instance_wrappers[next_instance_sdn]
-
-                        if next_instance.is_assign():
-                            continue
-
-                        self.log(f"  {next_instance.name}")
-                        if next_instance.is_register():
-                            if (
-                                next_instance not in instance_depth
-                                and next_instance not in starting_points
-                                and pin.inner_pin.port.name != "C"
-                            ):
-                                self.log("    Adding register")
-                                starting_points.add(next_instance)
-                            continue
-
-                        # If this has already been visited, don't visit again,
-                        # unless we are looking for max depth and the depth is going to increase
-                        if next_instance in instance_depth and (
-                            not max_not_min or instance_depth[next_instance] >= current_depth
-                        ):
-                            continue
-
-                        instances_to_visit.append(next_instance)
-                        instance_depth[next_instance] = current_depth
-                        self.log(f"    {next_instance.name} -> {instance_depth[next_instance]}")
-
-            current_depth += 1
+                    if next_instance.is_register():
+                        instance_depth[next_instance] = instance_depth[instance] + 1
+                    else:
+                        instance_depth[next_instance] = instance_depth[instance]
+                    instances_to_visit.append(next_instance)
+                    self.log(f"    {next_instance.name} -> {instance_depth[next_instance]}")
 
         self.log_title("Instance depths:")
         for instance, depth in instance_depth.items():
