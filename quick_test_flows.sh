eb8287245ed54b933975d90be093a73f5fbb055c
Jeff Goeders <jeff.goeders@gmail.com>
2023-11-28 11:07:37 -0700

    Remove duplicate flow

--- quick_test_flows.sh
+++ quick_test_flows.sh
@@ -7,7 +7,6 @@ python scripts/run.py vivado_bit_analysis designs/basic/and3
 python scripts/run.py vivado_conformal designs/basic/and3
 python scripts/run.py vivado_ooc designs/basic/and3
 python scripts/run.py vivado_phys_netlist_cmp designs/basic/and3
-python scripts/run.py vivado_phys_netlist_xrev designs/basic/and3
 python scripts/run.py vivado_phys_netlist designs/basic/and3
 python scripts/run.py vivado_structural_error_injection designs/basic/and3 --flow_arguments="{'num_runs': 5}"
 python scripts/run.py vivado_yosys_impl designs/basic/and3
