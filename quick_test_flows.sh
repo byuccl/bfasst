748c7ba40bdfd02615adc6aed1972b3af3b2fa23
Connor Young <yonnorc42@gmail.com>
2024-01-02 18:15:10 -0700

    Took out RandSoc since it's already being tested with RandSocDumped and changed VivadoYosysImpl to VivadoYosysCmp

--- quick_test_flows.sh
+++ quick_test_flows.sh
@@ -3,7 +3,6 @@
 set -e #exit on error
 
 python scripts/run.py RandSocDumped --flow_arguments="{'num_designs': 1}"
-python scripts/run.py RandSoc --flow_arguments="{'num_designs': 1}"
 python scripts/run.py Vivado designs/basic/and3
 python scripts/run.py VivadoBitToNetlist designs/basic/and3
 python scripts/run.py VivadoBitAnalysis designs/basic/and3
@@ -12,4 +11,4 @@ python scripts/run.py VivadoOoc designs/basic/and3
 python scripts/run.py VivadoPhysNetlist designs/basic/and3
 python scripts/run.py VivadoPhysNetlistCmp designs/basic/and3
 python scripts/run.py VivadoStructuralErrorInjection designs/basic/and3 --flow_arguments="{'num_runs': 5}"
-python scripts/run.py VivadoYosysImpl designs/basic/and3
+python scripts/run.py VivadoYosysCmp designs/basic/and3
