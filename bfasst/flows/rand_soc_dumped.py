4af69d5f691296e3d0f8e1215a5dd3915f35c354
ReillyMcK <romckend@byu.edu>
2024-02-22 20:29:19 -0700

    Remove VivadoSynthFromTcl

--- bfasst/flows/rand_soc_dumped.py
+++ bfasst/flows/rand_soc_dumped.py
@@ -1,10 +1,11 @@
 """Flow to dump bels from random soc block designs with Isoblaze"""
+
 import pathlib
 
 from bfasst.flows.flow import FlowNoDesign
 from bfasst.tools.design_create.rand_soc import RandSoC
 from bfasst.tools.impl.vivado_impl import VivadoImpl
-from bfasst.tools.synth.vivado_synth_tcl import VivadoSynthFromTcl
+from bfasst.tools.synth.vivado_synth import VivadoSynth
 from bfasst.tools.transform.randsoc_dump import RandsocDump
 
 
@@ -17,7 +18,8 @@ class RandSocDumped(FlowNoDesign):
         self.rand_soc_tool = RandSoC(self, num_designs=num_designs)
 
         for i, design in enumerate(self.rand_soc_tool.outputs["design_tcl"]):
-            VivadoSynthFromTcl(self, design)
+            synth_tool = VivadoSynth(self, design.parent)
+            synth_tool.synth_build["tcl_sources"] = [str(design)]
             VivadoImpl(self, design.parent)
             RandsocDump(
                 self,
