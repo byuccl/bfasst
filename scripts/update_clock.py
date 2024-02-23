5343fe574f7d00a9f30ffdcf0129cbbcda48e024
ReillyMcK <romckend@byu.edu>
2024-02-22 18:21:10 -0700

    Fix clock params

---
+++ scripts/update_clock.py
@@ -0,0 +1,54 @@
+"""
+Update design.yaml with the clock rate found in the timing summary report
+"""
+
+import argparse
+from bfasst import yaml_parser
+from bfasst.paths import BUILD_PATH
+from pathlib import Path
+
+
+def get_clock_period(timing_summary):
+    with open(timing_summary, "r") as f:
+        for line in f:
+            if "Requirement:" in line:
+                period = line.split()[1].strip()[:-2]
+                return period
+    return None
+
+
+def update_clocks(designs_yaml):
+    design_paths = yaml_parser.RunParser(designs_yaml).design_paths
+    for design in design_paths:
+        design = Path(design)
+        timing_summary = (
+            Path("/home/reilly/equiv/bfasst/build")
+            / design.parent.name
+            / design.name
+            / "impl/timing_summary.txt"
+        )
+        period = get_clock_period(timing_summary)
+        if period is None:
+            print(f"Warning: Could not find clock period in {timing_summary}")
+            continue
+        design_yaml = Path("/home/reilly/equiv/bfasst/designs") / design / "design.yaml"
+        with open(design_yaml, "r") as f:
+            lines = f.readlines()
+            for idx, line in enumerate(lines):
+                if "period:" in line:
+                    lines[idx] = f"    period: {period}\n"
+                    try:
+                        if "waveform" in lines[idx + 1]:
+                            lines.pop(idx + 1)
+                    except IndexError:
+                        pass
+                    break
+        with open(design_yaml, "w") as f:
+            f.writelines(lines)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("experiment_yaml", type=str, help="Path to yaml with list of designs")
+    args = parser.parse_args()
+    update_clocks(args.experiment_yaml)
