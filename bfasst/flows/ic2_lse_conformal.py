10df2eecfb600c0d5cd101b73ccd3a949c925f1d
KeenanRileyFaulkner <keenan.r.faulkner@gmail.com>
2024-03-05 18:14:25 -0700

    added icecube implementation for ninja

--- bfasst/flows/ic2_lse_conformal.py
+++ bfasst/flows/ic2_lse_conformal.py
@@ -7,6 +7,7 @@ from bfasst.paths import FLOWS_PATH
 # from bfasst.tools.impl.ic2_impl_tool import Ic2Impl
 # from bfasst.tools.rev_bit.icestorm import IcestormRevBit
 from bfasst.tools.synth.ic2_lse_synth import Ic2LseSynth
+from bfasst.tools.impl.ic2_impl import Ic2Impl
 
 # from bfasst.types import Vendor
 
@@ -17,19 +18,19 @@ class Ic2LseConformal(Flow):
     def __init__(self, design):
         super().__init__(design)
         self.ic2_lse_synth_tool = Ic2LseSynth(self, design)
-        # self.ic2_impl_tool = Ic2Impl(self, design)
+        self.ic2_impl_tool = Ic2Impl(self, design)
         # self.icestorm_reverse_bit_tool = IcestormRevBit(self, design)
         # self.conformal_tool = Conformal(self, design)
 
     def create_rule_snippets(self):
         self.ic2_lse_synth_tool.create_rule_snippets()
-        # self.ic2_impl_tool.create_rule_snippets()
+        self.ic2_impl_tool.create_rule_snippets()
         # self.icestorm_reverse_bit_tool.create_rule_snippets()
         # self.conformal_tool.create_rule_snippets()
 
     def create_build_snippets(self):
         self.ic2_lse_synth_tool.create_build_snippets()
-        # self.ic2_impl_tool.create_build_snippets()
+        self.ic2_impl_tool.create_build_snippets(self.ic2_lse_synth_tool.outputs["edif_file"])
         # self.icestorm_reverse_bit_tool.create_build_snippets(
         #     str(self.ic2_impl_tool.outputs["bitstream"])
         # )
@@ -41,7 +42,7 @@ class Ic2LseConformal(Flow):
 
     def add_ninja_deps(self, deps):
         self.ic2_lse_synth_tool.add_ninja_deps(deps)
-        # self.ic2_impl_tool.add_ninja_deps(deps)
+        self.ic2_impl_tool.add_ninja_deps(deps)
         # self.icestorm_reverse_bit_tool.add_ninja_deps(deps)
         # self.conformal_tool.add_ninja_deps(deps)
         deps.append(FLOWS_PATH / "ic2_lse_conformal.py")
