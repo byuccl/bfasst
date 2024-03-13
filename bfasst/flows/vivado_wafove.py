f2906fffc5652d643a5e8324e107d1d6cc616cbb
KeenanRileyFaulkner <keenan.r.faulkner@gmail.com>
2024-03-12 18:38:10 -0600

    updated wafove tools to take external inputs through constructor

--- bfasst/flows/vivado_wafove.py
+++ bfasst/flows/vivado_wafove.py
@@ -19,18 +19,22 @@ class VivadoWafove(Flow):
             self, design, prev_tool_outputs=self.vivado_synth_tool.outputs
         )
         self.xrev_tool = Xray(self, design, prev_tool_outputs=self.vivado_impl_tool.outputs)
-        self.wafove_tool = Wafove(self, design)
+        self.wafove_tool = Wafove(self, design, self.__create_wafove_inputs_dict())
 
-    def create_build_snippets(self):
-        self.vivado_synth_tool.create_build_snippets()
-        self.vivado_impl_tool.create_build_snippets()
-        self.xrev_tool.create_build_snippets()
-        self.wafove_tool.create_build_snippets(
-            netlist_a=self.vivado_impl_tool.outputs["impl_verilog"],
-            netlist_b=self.xrev_tool.outputs["xray_netlist"],
-            log_name="wafove.log",
-            std_err_log="wafove_error.log",
-        )
+        self.tools = [
+            self.vivado_synth_tool,
+            self.vivado_impl_tool,
+            self.xrev_tool,
+            self.wafove_tool,
+        ]
+
+    def __create_wafove_inputs_dict(self):
+        return {
+            "impl_tool": self.vivado_impl_tool.outputs,
+            "rev_bit_tool": self.xrev_tool.outputs,
+            "log_name": "wafove.log",
+            "std_err_log": "wafove_stderr.log",
+        }
 
     def get_top_level_flow_path(self):
         return pathlib.Path(__file__)
--- bfasst/tools/compare/wafove/wafove.py
+++ bfasst/tools/compare/wafove/wafove.py
@@ -6,22 +6,33 @@ from bfasst.tools.tool import Tool
 class Wafove(Tool):
     """Create the rule and build snippets for structural comparison."""
 
-    def __init__(self, flow, design):
+    def __init__(self, flow, design, prev_tool_outputs):
         super().__init__(flow, design)
+
+        # A dictionary of the previous tool outputs
+        # In this case, the dictionary will contain keys for
+        # 1. An implementation_tool golden_netlist
+        # 2. A reverse_bit_tool rev_netlist
+        # 3. A log_name
+        # 4. A std_err_log
+        self.prev_tool_outputs = prev_tool_outputs
+
         self.build_path = self.design_build_path / "wafove"
+        self._init_outputs(
+            self.prev_tool_outputs["log_name"], self.prev_tool_outputs["std_err_log"]
+        )
 
     def create_rule_snippets(self):
         self._append_rule_snippets_default(__file__)
 
-    def create_build_snippets(self, netlist_a, netlist_b, log_name, std_err_log):
-        self._init_outputs(log_name, std_err_log)
+    def create_build_snippets(self):
         self._append_build_snippets_default(
             __file__,
             render_dict={
                 "top": self.design_props.top,
                 "base_path": str(self.build_path),
-                "netlist_a": str(netlist_a),
-                "netlist_b": str(netlist_b),
+                "netlist_a": str(self.prev_tool_outputs["impl_tool"]["golden_netlist"]),
+                "netlist_b": str(self.prev_tool_outputs["rev_bit_tool"]["rev_netlist"]),
                 "log_path": str(self.outputs["wafove_log"]),
                 "std_err_log": str(self.outputs["std_err_log"]),
                 "reversed_tb": str(self.outputs["reversed_tb"]),
