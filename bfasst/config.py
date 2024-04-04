f06fbd44568a31403bc86f6aa2c9eee7c1e72fb7
KeenanRileyFaulkner <keenan.r.faulkner@gmail.com>
2024-04-03 18:28:32 -0600

    got synplify to run

--- bfasst/config.py
+++ bfasst/config.py
@@ -6,15 +6,18 @@ from pathlib import Path
 # Tool path locations
 IC2_INSTALL_DIR = Path("/tools/lscc/iCEcube2.2020.12")
 IC2_FOUNDRY = IC2_INSTALL_DIR / "LSE"
-IC2_LD_LIBRARY_PATH = IC2_FOUNDRY / "bin" / "lin64"
-IC2_SYNTH_BIN = IC2_LD_LIBRARY_PATH / "synthesis"
+IC2_LSE_LD_LIBRARY_PATH = IC2_FOUNDRY / "bin" / "lin64"
+IC2_LSE_SYNTH_BIN = IC2_LSE_LD_LIBRARY_PATH / "synthesis"
 IC2_SBT_DIR = IC2_INSTALL_DIR / "sbt_backend"
+IC2_SYNPLIFY_LD_LIBRARY_PATH = IC2_SBT_DIR / "bin" / "linux" / "opt" / "synpwrap"
+IC2_SYNPLIFY_PATH = IC2_INSTALL_DIR / "synpbase"
+IC2_SYNPLIFY_BIN = IC2_SYNPLIFY_LD_LIBRARY_PATH / "synpwrap"
 
 CONFORMAL_REMOTE_MACHINE = "ssh.et.byu.edu"
-CONFORMAL_REMOTE_MACHINE_USER = "jgoeders"
+CONFORMAL_REMOTE_MACHINE_USER = "keenanrf"
 CONFORMAL_REMOTE_SOURCE_SCRIPT = Path("/ee2/Cadence/local/designkits/ee451/cdssetup/bashrc_cadence")
-CONFORMAL_REMOTE_WORK_DIR = Path("/fsj/jgoeders/bfasst_work")
-CONFORMAL_REMOTE_LIBS_DIR = Path("/fsj/jgoeders/bfasst_libs")
+CONFORMAL_REMOTE_WORK_DIR = Path("/fsd/keenanrf/bfasst_work")
+CONFORMAL_REMOTE_LIBS_DIR = Path("/fsd/keenanrf/bfasst_libs")
 CONFORMAL_REMOTE_PATH = Path("/ee2/Cadence/CONFRML152/bin/lec")
 
 VIVADO_BIN_PATH = Path(os.environ["VIVADO_PATH"])
