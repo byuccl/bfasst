5d706413c831f7f8b7463dad7d76fa3ea559512c
KeenanRileyFaulkner <keenan.r.faulkner@gmail.com>
2024-04-11 22:17:29 -0600

    removed deleted tools from init

--- bfasst/tools_legacy/synth/__init__.py
+++ bfasst/tools_legacy/synth/__init__.py
@@ -3,4 +3,3 @@ import bfasst.tools_legacy.synth.base
 import bfasst.tools_legacy.synth.ic2_lse
 import bfasst.tools_legacy.synth.ic2_synplify
 import bfasst.tools_legacy.synth.yosys
-import bfasst.tools_legacy.synth.vivado
