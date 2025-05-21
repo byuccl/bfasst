35d9f153d72d3b44c39ee3d0ebdfec5763f9b501
Connor Young <yonnorc42@gmail.com>
2025-05-21 13:11:45 -0600

    add processing for new comparison flow

--- scripts/process_results.py
+++ scripts/process_results.py
@@ -144,6 +144,78 @@ def transform_stats(designs_yaml):
         row += 1
     workbook.close()
 
+def phys_netlist_results(flow, out="results.csv"):
+    """
+    Gather results from phys_cmp flow and create a CSV file with
+    utilization data, transformation times, comparison times, and
+    success status.
+    """
+    root_dir = ROOT_PATH
+
+    fl = yaml_parser.RunParser(flow)
+
+    designs = [
+        BUILD_PATH / Path(design).parent.name / Path(design).name for design in fl.design_paths
+    ]
+
+    rows = []
+    for design in designs:
+        status = (
+            "Success" if (design / "vivado_phys_netlist/transformation_time.txt").exists() else "FAILED"
+        )
+        row = {
+            "Design": design.name,
+            "Status": status,
+            "LUT": 0,
+            "LUT_MEM": 0,
+            "SRL": 0,
+            "FF": 0,
+            "CARRY4": 0,
+            "BRAM": 0,
+            "P_TIME": 0,
+        }
+        if status == "FAILED":
+            rows.append(row)
+            continue
+        utilization_file = design / "vivado_impl/utilization.txt"
+        if not utilization_file.is_file():
+            continue
+        with open(utilization_file, "r") as f:
+            for line in f:
+                if "| LUT as Logic" in line:
+                    row["LUT"] = line.split("|")[2].strip()
+                elif "|   LUT as Distributed RAM" in line:
+                    row["LUT_MEM"] = line.split("|")[2].strip()
+                elif "|   LUT as Shift Register" in line:
+                    row["SRL"] = line.split("|")[2].strip()
+                elif "| Slice Registers" in line:
+                    row["FF"] = line.split("|")[2].strip()
+                elif "| CARRY4" in line:
+                    row["CARRY4"] = line.split("|")[2].strip()
+                elif "| Block RAM Tile" in line:
+                    row["BRAM"] = line.split("|")[2].strip()
+
+        with open(design / "vivado_phys_netlist/transformation_time.txt", "r") as f:
+            row["P_TIME"] = round(float(f.read().strip()), 2)
+        rows.append(row)
+
+    with open(out, "w", newline="") as f:
+        fieldnames = [
+            "Design",
+            "Status",
+            "LUT",
+            "LUT_MEM",
+            "SRL",
+            "FF",
+            "CARRY4",
+            "BRAM",
+            "P_TIME",
+        ]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+
 
 def phys_cmp_results(flow):
     """
